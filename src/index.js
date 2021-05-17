b75a2d72be5e8f5eaaa98ed79e311b035aa854f3
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-05-17 13:56:08 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import countTimer from './modules/countTimer';
+import toggleMenu from './modules/toggleMenu';
+import togglePopup from './modules/togglePopup';
+import tabs from './modules/tabs';
+import slider from './modules/slider';
+import sendForm from './modules/sendForm';
+import calc from './modules/calc';
+import addDot from './modules/addDot';
+import changeImages from './modules/changeImages';
+import validateInputs from './modules/validateInputs';
+
+countTimer('31 may 2021');
+validateInputs();
+toggleMenu();
+togglePopup()
+tabs();
+addDot();
+slider();
+calc(100);
+sendForm();
+changeImages();
